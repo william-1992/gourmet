48673fa7f19d52003be23191aa04caaa28a7b83e
william-1992 <malianwei1992@126.com>
2022-09-04 15:59:44 +0800

    fix: [我的] 添加公司名称+电话

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -33,21 +33,31 @@ export default class User extends Component {
     })
   }
 
+  callPhone = () => {
+    window.location.href = 'tel:13970029205'
+  }
+
   render () {
     return (
       <View className='user-wrap'>
-        <View className='user-top'>
-          <Image src={avatar}></Image>
-          <Text>美食家店家<Image onClick={this.toEdit} src={edit}></Image></Text>
+        <View className='user-content'>
+          <View className='user-top'>
+            <Image src={avatar}></Image>
+            <Text>美食家店家<Image onClick={this.toEdit} src={edit}></Image></Text>
+          </View>
+          <AtList>
+            <AtListItem title='收货姓名' extraText='老马' />
+            <AtListItem title='收货电话' extraText='美食家' />
+            <AtListItem title='收货地址' extraText='13012341234' />
+          </AtList>
+          <AtList className='user-help'>
+            <AtListItem title='客服与帮助' arrow='right' onClick={this.callPhone} />
+          </AtList>
         </View>
-        <AtList>
-          <AtListItem title='收货姓名' extraText='老马' />
-          <AtListItem title='收货电话' extraText='美食家' />
-          <AtListItem title='收货地址' extraText='13012341234' />
-        </AtList>
-        <AtList className='user-help'>
-          <AtListItem title='客服与帮助' arrow='right' />
-        </AtList>
+        <View className='user-foot'>
+          {/* <a href="tel:12345678902">联系客服</a> */}
+          <Text>技术支持：河北点闪科机有限公司</Text>
+        </View>
       </View>
     )
   }
